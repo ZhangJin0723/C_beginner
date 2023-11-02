28099de598f369d1bcf474758fccb5dce99a8a82
ZhangJin0723 <18733668979@163.com>
2023-11-02 23:20:22 +0800

    最小值

---
+++ 11_02/3_6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("请输入三个整数。\n");
+    printf("整数A:");
+    scanf("%d",&a);
+    printf("整数B:");
+    scanf("%d",&b);
+    printf("整数C:");
+    scanf("%d",&c);
+    if (a<b & a<c) printf("最小值为%d",a);
+    else if (b<a & b<c) printf("最小值为%d",b);
+    else printf("最小值为%d",c);
+    return 0;
+
+}
