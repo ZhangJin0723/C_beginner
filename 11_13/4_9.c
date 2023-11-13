79e9f02437f14e223038f5f71d6b562b0d17134b
ZhangJin0723 <18733668979@163.com>
2023-11-13 22:28:33 +0800

    while

---
+++ 11_13/4_9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int no;
+    printf("正整数：");
+    scanf("%d",&no);
+
+    while (no>0){
+        putchar('+');
+        no--;
+       if(--no>0){
+            putchar('-');
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
